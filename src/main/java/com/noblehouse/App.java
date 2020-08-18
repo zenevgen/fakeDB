803a86f884c76fdd9e1f573555f65a788080da6a
Evgeniy Baraulya <evgeniy.baraulya@gmail.com>
2020-08-18 22:42:58 +0300

    final commit

--- src/main/java/com/noblehouse/App.java
+++ src/main/java/com/noblehouse/App.java
@@ -21,17 +21,18 @@ public class App {
                 qtyMist = Integer.parseInt(args[2]);
             }
             for (int i = 0; i < count; i++) {
-                System.out.println(qtyMist);
                 finalPerson = fakePerson();
-                int variety = random.nextInt(2);
+                int variety = random.nextInt(3);
                 if (variety == 0) {
                     finalPerson = withRemoves(finalPerson);
-                } else {
+                } else if (variety == 1) {
                     finalPerson = withReplc(finalPerson);
+                } else {
+                    finalPerson = withInsert(finalPerson);
                 }
                 System.out.println(finalPerson);
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Something went wrong");
             System.out.println(e.getMessage());
             System.out.println("Please make sure you enter correct input: e.g. ru 10 25");
@@ -49,23 +50,35 @@ public class App {
 
     public static String withRemoves(String person) {
         String modString1 = person;
-        System.out.println(qtyMist);
         for (int i = 0; i < qtyMist; i++) {
-            int firstChar = random.nextInt(modString1.length() - 1);
+            int firstChar = random.nextInt(modString1.length());
             int secondChar = firstChar + 1;
             modString1 = modString1.replaceFirst(modString1.substring(firstChar, secondChar), "");
+            if (modString1.length() < 1) {
+                break;
+            }
         }
         return modString1;
     }
 
     public static String withReplc(String person) {
-        String modString2=person;
-        System.out.println(qtyMist);
+        String modString2 = person;
         for (int i = 0; i < qtyMist; i++) {
             int firstChar = random.nextInt(modString2.length() - 1);
             int secondChar = firstChar + 1;
-            modString2 = person.replaceFirst(modString2.substring(firstChar, secondChar), modString2.substring(secondChar, secondChar + 1));
+            modString2 = modString2.replaceFirst(modString2.substring(firstChar, secondChar), modString2.substring(secondChar, secondChar + 1));
         }
         return modString2;
+    }
+
+    public static String withInsert(String person) {
+        StringBuffer intermedString = new StringBuffer(person);
+        for (int i = 0; i < qtyMist; i++) {
+            int position = random.nextInt(intermedString.length());
+            char charForIns = person.charAt(random.nextInt(person.length()));
+            intermedString = intermedString.insert(position, charForIns);
+        }
+        String modString3 = intermedString.toString();
+        return modString3;
     }
 }
